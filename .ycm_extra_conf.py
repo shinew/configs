9be92b02df717da1bcef87246d6b36c71e56f846
Shine Wang <shine.sw.wang@gmail.com>
2017-11-11 14:18:49 -0500

    z

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -32,7 +32,7 @@ flags = [
 '-isystem',
 '/usr/local/include',
 '-isystem',
-'/Library/Developer/CommandLineTools/usr/bin/../lib/clang/9.0.0/include',
+'/Library/Developer/CommandLineTools/usr/lib/clang/9.0.0/include',
 '-isystem',
 '/Library/Developer/CommandLineTools/usr/include',
 '-isystem',
